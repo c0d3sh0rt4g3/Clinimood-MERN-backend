0a6898a685f220e7e0ce8ea51ee22b09dafa8f23
out0f4mm0 <eriking2004@gmail.com>
2025-01-13 13:39:52 +0100

    MAde appointment.model.js

---
+++ models/appointment.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+// Medical appointments DB shema
+const appointmentSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    description: String,
+    status: String
+}, {
+    timestamps: true
+})
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+module.exports = Appointment;
