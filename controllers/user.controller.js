940727fc64a3ca8b0e389f4a5fb69d42bf2059fc
out0f4mm0 <eriking2004@gmail.com>
2025-01-13 12:17:54 +0100

    Made updateByDni endpoint

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -40,6 +40,28 @@ export const createUser = async (req, res) => {
     }
 }
 
+export const updateUserByDni = async (req, res) => {
+    const { dni } = req.params
+    const updates = req.body
+
+    if (!updates.DNI || !updates.name || !updates.email || !updates.password || !updates.role) {
+        res.status(400).json({success: false, error: "DNI, name, email, password and role are required fields"})
+    }
+
+    try {
+        const updatedUser = await  User.findOneAndUpdate({DNI : dni}, updates, {new : true})
+
+        if (!updatedUser) {
+            return res.status(404).json({success: false, error: `User with dni ${dni} not found`})
+        }
+
+        res.status(200).json({success: true, data: updatedUser})
+    } catch (error){
+        console.error(`Error in updating user: ${error.message}`)
+        res.status(500).json({success: false, error: error.message})
+    }
+}
+
 export const deleteUser = async (req, res) => {
     const { dni } = req.params;
     try {
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createUser, deleteUser, getAllUsers, getUserByDni} from "../controllers/user.controller.js";
+import {createUser, deleteUser, getAllUsers, getUserByDni, updateUserByDni} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.get("/", getAllUsers)
 router.get("/:dni", getUserByDni)
 
 router.post("/", createUser)
+
+router.put("/:dni", updateUserByDni)
 
 router.delete("/:dni", deleteUser)
 
