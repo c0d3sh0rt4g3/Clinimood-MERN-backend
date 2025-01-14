875f97f1d57d93d7e0579b2c7a30bd8f620b0630
out0f4mm0 <eriking2004@gmail.com>
2025-01-14 14:24:02 +0100

    Made deleteHistory endpoint

--- routes/history.route.js
+++ routes/history.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {createMedicalHistory, getAllHistories} from "../controllers/history.controller.js";
+import {createMedicalHistory, deleteHistory, getAllHistories} from "../controllers/history.controller.js";
 
 const router = express.Router();
 
 router.get("/", getAllHistories)
 
 router.post("/", createMedicalHistory)
+
+router.delete("/:patientDNI", deleteHistory)
 
 export default router;
