d1e086834f276c62e997545607d7904af7915504
out0f4mm0 <eriking2004@gmail.com>
2025-01-15 13:42:59 +0100

    Installed CORS dependency

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import express from "express";
-import cors from "cors"; // Importa el middleware CORS
+import cors from "cors";
 import userRoutes from "./routes/user.route.js";
 import appointmentRoutes from "./routes/appointment.route.js";
 import historyRoutes from "./routes/history.route.js";
