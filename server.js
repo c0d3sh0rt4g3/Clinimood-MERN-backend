e1cd7fafe68c2a8b66e10196942646212efb1261
out0f4mm0 <eriking2004@gmail.com>
2025-01-15 13:37:05 +0100

    Added CORS config

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-import {connectDB} from "./config/db.js";
+import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors"; // Importa el middleware CORS
 import userRoutes from "./routes/user.route.js";
 import appointmentRoutes from "./routes/appointment.route.js";
 import historyRoutes from "./routes/history.route.js";
 
-const app = express()
-app.use(express.json());
 dotenv.config();
 
+const app = express();
+app.use(express.json());
+
+// Configure CORS
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use('/users', userRoutes);
-app.use('/appointments', appointmentRoutes)
-app.use('/history', historyRoutes)
+app.use('/appointments', appointmentRoutes);
+app.use('/history', historyRoutes);
 
 app.listen(5000, () => {
-    connectDB()
+    connectDB();
     console.log('Server started at http://localhost:5000');
 });
