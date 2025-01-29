720c55f80ff935f3d6a2b7f0d6aacc407fc8b443
out0f4mm0 <eriking2004@gmail.com>
2025-01-29 12:45:29 +0100

    Fixed minor issue on cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // Configure CORS
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || "http://localhost:5000" || "http://localhost:5000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173" || "http://localhost:5000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 };
