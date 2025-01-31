f01f60f175f923a594931950f3765fcda0f03d72
out0f4mm0 <eriking2004@gmail.com>
2025-01-31 12:16:42 +0100

    Documented user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,22 @@
 import mongoose from "mongoose";
 
-// User DB schema
+/**
+ * User DB schema.
+ * @typedef {Object} UserSchema
+ * @property {string} DNI - DNI of the user (required, unique).
+ * @property {string} name - Name of the user (required).
+ * @property {string} email - Email of the user (required, unique).
+ * @property {string} password - Password of the user (required).
+ * @property {string} role - Role of the user. Must be one of: 'patient', 'doctor', 'admin' (required).
+ * @property {string} [phone] - Phone number of the user (optional).
+ * @property {string} [address] - Address of the user (optional).
+ * @property {string} [specialization] - Specialization of the user (optional, typically for doctors).
+ * @property {string} [profilePicture] - URL or path to the user's profile picture (optional).
+ * @property {string} [resetPasswordToken] - Hashed token for password reset (optional).
+ * @property {Date} [resetPasswordExpires] - Expiration time for the password reset token (optional).
+ * @property {Date} createdAt - Timestamp of when the user was created (automatically added).
+ * @property {Date} updatedAt - Timestamp of when the user was last updated (automatically added).
+ */
 const userSchema = new mongoose.Schema({
     DNI: {
         type: String,
@@ -29,16 +45,16 @@ const userSchema = new mongoose.Schema({
     address: String,
     specialization: String,
     profilePicture: String,
-
-    // Stores the hashed reset token
     resetPasswordToken: String,
-    // Stores the expiration time of the token
     resetPasswordExpires: Date
-
 }, {
     timestamps: true
 });
 
+/**
+ * Mongoose model for the User schema.
+ * @type {mongoose.Model<UserSchema>}
+ */
 const User = mongoose.model('User', userSchema);
 
 export default User;
