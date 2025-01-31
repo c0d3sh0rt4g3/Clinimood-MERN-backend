b09bc5fa396caabe31f4782f826e6d5e6ba17fa8
out0f4mm0 <eriking2004@gmail.com>
2025-01-31 10:26:58 +0100

    Documented appointment.model.js

--- models/appointment.model.js
+++ models/appointment.model.js
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
 
-// Medical appointments DB schema
+/**
+ * Medical appointments DB schema.
+ * @typedef {Object} AppointmentSchema
+ * @property {string} patientDNI - DNI of the patient (required).
+ * @property {string} doctorDNI - DNI of the doctor (required).
+ * @property {Date} date - Date and time of the appointment (required).
+ * @property {string} [description] - Optional description of the appointment.
+ * @property {string} status - Status of the appointment. Must be one of: 'pending', 'completed', 'confirmed'.
+ * @property {Date} createdAt - Timestamp of when the appointment was created (automatically added).
+ * @property {Date} updatedAt - Timestamp of when the appointment was last updated (automatically added).
+ */
 const appointmentSchema = new mongoose.Schema({
     patientDNI: {
         type: String,
@@ -22,8 +32,12 @@ const appointmentSchema = new mongoose.Schema({
     }
 }, {
     timestamps: true
-})
+});
 
+/**
+ * Mongoose model for the Appointment schema.
+ * @type {mongoose.Model<AppointmentSchema>}
+ */
 const Appointment = mongoose.model('Appointment', appointmentSchema);
 
 export default Appointment;
