b2b9da85d8b1614007462b109cefe69650b62e60
out0f4mm0 <eriking2004@gmail.com>
2025-01-31 11:55:16 +0100

    Documented history.model.js

--- src/models/history.model.js
+++ src/models/history.model.js
@@ -1,19 +1,30 @@
 import mongoose from "mongoose";
 
-// Medical history DB schema
+/**
+ * Medical history DB schema.
+ * @typedef {Object} HistorySchema
+ * @property {string} patientDNI - DNI of the patient (required).
+ * @property {Array.<mongoose.Types.ObjectId>} appointmentIdArray - Array of appointment IDs associated with the patient.
+ * @property {Date} createdAt - Timestamp of when the history was created (automatically added).
+ * @property {Date} updatedAt - Timestamp of when the history was last updated (automatically added).
+ */
 const historySchema = new mongoose.Schema({
     patientDNI: {
         type: String,
         required: true
     },
-    appointmentIdArray: [{ // Renamed field for clarity
+    appointmentIdArray: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Appointment'
+        ref: 'Appointment' // Reference to the Appointment model
     }]
 }, {
-    timestamps: true
+    timestamps: true // Automatically adds createdAt and updatedAt fields
 });
 
+/**
+ * Mongoose model for the History schema.
+ * @type {mongoose.Model<HistorySchema>}
+ */
 const History = mongoose.model('History', historySchema);
 
 export default History;
